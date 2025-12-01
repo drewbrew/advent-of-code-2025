f09fe5a339d245bd1849c9d1a6007a7d76b3de4d
Drew Winstel <drew@hsv.beer>
2025-12-01 08:06:07 -0600

    refactor: both parts in one puzzle

--- day01.py
+++ day01.py
@@ -14,42 +14,32 @@ R14
 L82""".splitlines()
 
 
-def part_1(puzzle_input: list[str], start: int = 50) -> int:
+def run_puzzle(puzzle_input: list[str], start: int = 50) -> tuple[int, int]:
     """How many times does the pudialzzle point at 0?"""
     dial = deque(range(100))
     dial.rotate(start)
-    count = 0
-    for line in puzzle_input:
-        amount = int(line[1:]) * (-1 if line[0] == "L" else 1)
-        dial.rotate(amount)
-        if dial[0] == 0:
-            count += 1
-    return count
-
-
-def part_2(puzzle_input: list[str], start: int = 50) -> int:
-    """How many times does the dial cross 0?"""
-    dial = deque(range(100))
-    dial.rotate(start)
-    count = 0
+    p1_count = 0
+    p2_count = 0
     for line in puzzle_input:
         amount = int(line[1:])
         tick = -1 if line[0] == "L" else 1
         for _ in range(amount):
             dial.rotate(tick)
             if dial[0] == 0:
-                count += 1
-    return count
+                p2_count += 1
+        if dial[0] == 0:
+            p1_count += 1
+    return p1_count, p2_count
 
 
 def main():
-    part_one_result = part_1(TEST_INPUT)
+    part_one_result, part_two_result = run_puzzle(TEST_INPUT)
     assert part_one_result == 3, part_one_result
     real_input = Path("day01.txt").read_text().splitlines()
-    print(part_1(real_input))
-    part_two_result = part_2(TEST_INPUT)
     assert part_two_result == 6, part_two_result
-    print(part_2(real_input))
+    part_one_result, part_two_result = run_puzzle(real_input)
+    print(part_one_result)
+    print(part_two_result)
 
 
 if __name__ == "__main__":
