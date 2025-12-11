3b9e65c0d7fc9edaed8e75593a057460e3b2a45c
Drew Winstel <drew@hsv.beer>
2025-12-11 08:27:21 -0600

    Day 11: fast solution using recursion and looking at the graph

--- day11.py
+++ day11.py
@@ -1,5 +1,6 @@
 import multiprocessing
 from pathlib import Path
+import subprocess
 
 import networkx
 
@@ -40,13 +41,36 @@ def part_one(puzzle: str) -> int:
     return sum(1 for _ in networkx.all_simple_paths(graph, "you", "out"))
 
 
-def paths_from_a_to_b_excluding_c(graph: networkx.DiGraph, source: str, dest: str, excluded_node: str) -> int:
-    result = 0
-    for path in networkx.all_simple_paths(graph, source, dest):
-        if excluded_node not in path:
-            result += 1
-    print(f'from {source} to {dest} excluding {excluded_node} = {result}')
+def paths_from_a_to_b(
+    graph: networkx.DiGraph, source: str, dest: str, cache: dict[tuple[str, str], int],
+) -> int:
+    # time for some recursion!
+    # print(f'{source=}, {dest=}')
+    if source == dest:
+        # print('end')
+        return 1
+    try:
+        result = cache[source, dest]
+    except KeyError:
+
+        result = sum(
+            paths_from_a_to_b(graph, node2, dest, cache)
+            for node1, node2 in graph.edges
+            if node1 == source
+        )
+        cache[source, dest] = result
+
+    # if source in ['svr', 'dac', 'fft']:
+    #     print(f'{source} -> {dest} = {result}')
     return result
+
+
+def graph_to_dot(graph: networkx.Graph) -> str:
+    output = ["graph {"]
+    for node1, node2 in graph.edges:
+        output.append(f"    {node1} -- {node2}")
+    output.append("}")
+    return "\n".join(output)
 
 
 def part_two(puzzle: str) -> int:
@@ -57,28 +81,39 @@ def part_two(puzzle: str) -> int:
         source, dests = line.split(": ")
         for dest in dests.split():
             graph.add_edge(source, dest)
+    if puzzle != PART_TWO_TEST_INPUT:
+        dotfile = "day11.dot"
+        Path(dotfile).write_text(graph_to_dot(graph))
+        try:
+            output = subprocess.run(["dot", "-Tsvg", "day11.dot"], capture_output=True)
+        except FileNotFoundError:
+            print("Unable to write svg. Install graphviz and try again")
+        else:
+            assert not output.stderr, output.stderr
+            Path("day11.svg").write_bytes(output.stdout)
+            print("take a look at day11.svg")
+            print("if fft is below dac, everything here will break")
+
     # need to look at:
-    # svr -> fft (without hitting dac) -> dac -> out
-    # svr -> dac (without hitting fft) -> dac -> out
+    # svr -> fft (without hitting dac) -> dac -> out (possible)
+    # svr -> dac (without hitting fft) -> fft -> out (impossible)
     result = 0
-    svr_paths_to_fft = svr_paths_to_dac = dac_paths_to_fft = fft_paths_to_dac = (
-        fft_paths_to_out
-    ) = dac_paths_to_out = 0
+    svr_paths_to_fft = fft_paths_to_dac = dac_paths_to_out = 0
+    manager = multiprocessing.Manager()
+    cache = manager.dict()
     args_list = [
-        (graph, 'svr', 'dac', 'fft'),
-        (graph, 'svr', 'fft', 'dac'),
-        (graph, 'dac', 'fft', 'out'),
-        (graph, 'fft', 'dac', 'out'),
-        (graph, 'dac', 'out', 'fft'),
-        (graph, 'fft', 'out', 'dac'),
+        (graph, "svr", "fft", cache),
+        (graph, "fft", "dac", cache),
+        (graph, "dac", "out", cache),
     ]
     with multiprocessing.Pool() as pool:
-        (svr_paths_to_dac, svr_paths_to_fft, dac_paths_to_fft, fft_paths_to_dac, dac_paths_to_out, fft_paths_to_out) = pool.starmap(
-            paths_from_a_to_b_excluding_c, args_list
-        )
-    result = (svr_paths_to_dac * dac_paths_to_fft * fft_paths_to_out) + (
-        svr_paths_to_fft * fft_paths_to_dac * dac_paths_to_out
-    )
+        (
+            svr_paths_to_fft,
+            fft_paths_to_dac,
+            dac_paths_to_out,
+        ) = pool.starmap(paths_from_a_to_b, args_list)
+    result = svr_paths_to_fft * fft_paths_to_dac * dac_paths_to_out
+
     return result
 
 
